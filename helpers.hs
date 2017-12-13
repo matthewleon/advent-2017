443a63122829a85a08db2682c1ebc633ccf7cc70
Matthew Leon <ml@matthewleon.com>
2017-12-12 20:32:59 -0500

    some helper fns

---
+++ helpers.hs
@@ -0,0 +1,23 @@
+-- some helper functions to copy and paste in
+
+import Data.Char (isAlphaNum, isDigit)
+
+readInput :: IO String
+readInput = readFile "input"
+
+readInputLines :: IO [String]
+readInputLines = lines <$> readInput
+
+alphaNumStrings :: String -> [String]
+alphaNumStrings [] = []
+alphaNumStrings s =
+  let (junk, startAlphaNum) = break isAlphaNum s
+      (alphaNum, rest)      = span isAlphaNum startAlphaNum
+  in  alphaNum : alphaStrings rest
+
+ints :: String -> [Int]
+ints [] = []
+ints s =
+  let (junk, startNum) = break isDigit s
+      (num, rest)      = span isDigit startNum
+  in  read alphaNum : ints rest
